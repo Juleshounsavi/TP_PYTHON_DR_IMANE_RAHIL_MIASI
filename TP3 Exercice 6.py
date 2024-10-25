2f8bb6de106e1295858afe62e5501707023c1063
HOUNSAVI Jules koffi <114362041+Juleshounsavi@users.noreply.github.com>
2024-10-25 20:48:36 +0100

    Add files via upload

---
+++ TP3 Exercice 6.py
@@ -0,0 +1,6 @@
+
+n = 9
+for i in range(0,11):
+    print("{}*{} = {}".format(i, n, i*n))
+    
+    
---
+++ TP3 Exercice 7.py
@@ -0,0 +1,6 @@
+
+somme = 0
+for i in range(1,11):
+    somme = somme+i
+    
+print("Somme:", somme)    
